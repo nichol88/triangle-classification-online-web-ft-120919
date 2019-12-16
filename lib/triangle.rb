c8c2f9d7afac6c9f2139444b699c15613efea7a6
Brian Nicholls <nichol88@gmail.com>
2019-12-16 03:33:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
 
   def kind
     raise TriangleError if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 ||
-    @side1 > (@side2 + @side3) ||
-    @side2 > (@side1 + @side3) ||
-    @side3 > (@side2 + @side1)
+    @side1 >= (@side2 + @side3) ||
+    @side2 >= (@side1 + @side3) ||
+    @side3 >= (@side2 + @side1)
 
     sides = [@side1, @side2, @side3].sort
     #raise TriangleError if sides[2] < (sides[1] + sides[0])
